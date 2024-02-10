e111c11e76b5660d1eda43c034e44a23b3631fa9
RechidiAhmedAbdelaaziz <aa.rechidi@esi-sba.dz>
2024-02-10 12:05:05 +0100

    Create : Message model

--- src/models/index.ts
+++ src/models/index.ts
@@ -3,3 +3,6 @@ export * from "./appointment.model";
 export * from "./comment";
 export * from "./post";
 export * from "./question";
+export * from "./conversation.presonal";
+export * from "./conversation.medical";
+export * from "./message";
